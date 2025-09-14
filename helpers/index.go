fa98f6970b4c5f2e7aa9eee7b0cdeb86e2d0ce00
李悠然 <liyouran17sui@outlook.com>
2025-09-14 21:02:49 +0800

    在子包中调用实际函数逻辑

---
+++ helpers/index.go
@@ -0,0 +1,9 @@
+package helpers
+
+func Reverse(string2 string) string {
+	runes := []rune(string2)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
