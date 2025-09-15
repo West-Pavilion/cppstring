ec443c1d18d2ad1a0438976eaa59f53966cbe210
李悠然 <liyouran17sui@outlook.com>
2025-09-15 19:40:46 +0800

    新增 cppstring 结构体

--- cppstring.go
+++ cppstring.go
@@ -1,6 +1,14 @@
 package cppstring
 
-import "github.com/West-Pavilion/cppstring/helpers"
+import "github.com/West-Pavilion/cppstring/v2/helpers"
+
+type cppstring struct {
+	rawString string
+}
+
+func (cstring *cppstring) append(otherString *cppstring) cppstring {
+	return cppstring{string(append([]rune(cstring.rawString), []rune(otherString.rawString)...))}
+}
 
 func Reverse(raw string) string {
 	return helpers.Reverse(raw)
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/West-Pavilion/cppstring
+module github.com/West-Pavilion/cppstring/v2
 
 go 1.20
